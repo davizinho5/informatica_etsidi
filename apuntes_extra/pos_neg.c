801360482a2fed3eeccb66243cebebedc1818958
davizinho5 <dalvar_5@yahoo.es>
2025-02-28 15:05:54 +0100

    Update pos_neg.c

--- apuntes_extra/pos_neg.c
+++ apuntes_extra/pos_neg.c
@@ -4,21 +4,21 @@ int main()
 {
     int numero;
 
-    // Pedir al usuario un número
+    // Pedir al usuario un nÃºmero
     printf("Ingrese un numero entero: ");
     scanf("%i", &numero);
 
     // Determinar si es positivo, negativo o cero
     if (numero > 0) 
-		{
+    {
         printf("El numero es positivo.\n");
     } 
-		else if (numero < 0) 
-		{
+    else if (numero < 0) 
+    {
         printf("El numero es negativo.\n");
     } 
-		else 
-		{
+    else 
+    {
         printf("El numero es cero.\n");
     }
 
