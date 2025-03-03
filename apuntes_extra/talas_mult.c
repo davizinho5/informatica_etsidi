bbb8bb80676ef3e53e42e0ccbc66d747dd901e8c
davizinho5 <dalvar_5@yahoo.es>
2025-03-03 16:10:16 +0100

    Update talas_mult.c

--- apuntes_extra/talas_mult.c
+++ apuntes_extra/talas_mult.c
@@ -2,17 +2,17 @@
 
 int main() 
 {
-	int i,j;
-	// Bucle externo: Tablas del 1 al 9
+    int i,j;
+    // Bucle externo: Tablas del 1 al 9
     for (i = 1; i <= 9; i++) 
-	{ 
+    { 
         printf("Tabla del %i:\n", i);
-        // Bucle interno: Multiplicación del 1 al 10
+        // Bucle interno: MultiplicaciÃ³n del 1 al 10
         for (j = 1; j <= 10; j++) 
-		{ 
+        { 
             printf("%i x %i = %i\n", i, j, i * j);
         }
-        // Salto de línea entre tablas
+        // Salto de lÃ­nea entre tablas
         printf("\n");
     }
 
