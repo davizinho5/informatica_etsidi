f332fcce698dd67797ebb7a6b01fc24112d5223c
davizinho5 <dalvar_5@yahoo.es>
2025-03-03 16:09:44 +0100

    Update suma_naturales.c

--- apuntes_extra/suma_naturales.c
+++ apuntes_extra/suma_naturales.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Use un bucle FOR para calcular la suma de 
-// los 9 primeros números naturales
+// los 9 primeros nÃºmeros naturales
 
 int main() 
 {
@@ -9,11 +9,11 @@ int main()
     int i;
     
     for (i = 1; i <= 9; i++) 
-	{
+    {
         suma = suma + i;
     }
 
-    printf("La suma de los 9 primeros números naturales es: %d\n", suma);
+    printf("La suma de los 9 primeros nÃºmeros naturales es: %d\n", suma);
     
     return 0;
 }
