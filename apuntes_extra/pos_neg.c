8617c17c6d8c467676dc364116b301c386d722a1
davizinho5 <dalvar_5@yahoo.es>
2025-03-03 15:48:22 +0100

    Update pos_neg.c

--- apuntes_extra/pos_neg.c
+++ apuntes_extra/pos_neg.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Determinar si un número entero introducido por el ususario es positivo, negativo, o cero. 
+// Se debe hacer uso de la estructura if-else if
 int main() 
 {
     int numero;
@@ -8,15 +10,17 @@ int main()
     printf("Ingrese un numero entero: ");
     scanf("%i", &numero);
 
-    // Determinar si es positivo, negativo o cero
+    // Determinar si es positivo
     if (numero > 0) 
     {
         printf("El numero es positivo.\n");
     } 
+    // Si no, determinar si es negativo
     else if (numero < 0) 
     {
         printf("El numero es negativo.\n");
     } 
+    // Si no, solo queda la opción de que valga cero
     else 
     {
         printf("El numero es cero.\n");
