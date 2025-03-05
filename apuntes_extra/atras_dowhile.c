74f44658c198f0a5a7b95f8b16f54508df7aa246
davizinho5 <dalvar_5@yahoo.es>
2025-03-05 18:17:31 +0100

    Update atras_dowhile.c

--- apuntes_extra/atras_dowhile.c
+++ apuntes_extra/atras_dowhile.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	// Expresión de inicio
-	int i = 10;
-	// Use un bucle DO WHILE para imprimir una cuenta atras
-	// que empiece en 10 y acabe en 1
-  do
-  {
- 		printf("%i\n", i); 	
- 		i = i - 1; // Equivalente a: i--;
-	}while(i>0);
-	
-	return 0;
+   // ExpresiÃ³n de inicio
+   int i = 10;
+   // Use un bucle DO WHILE para imprimir una cuenta atras
+   // que empiece en 10 y acabe en 1
+   do
+   {
+      printf("%i\n", i);    
+      i = i - 1; // Equivalente a: i--;
+   }while(i>0);
+   
+   return 0;
 }
