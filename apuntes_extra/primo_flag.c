ab2651eb4a4d4c229150a44e1a7557de866239b5
davizinho5 <dalvar_5@yahoo.es>
2025-03-10 20:24:20 +0100

    Update primo_flag.c

--- apuntes_extra/primo_flag.c
+++ apuntes_extra/primo_flag.c
@@ -4,36 +4,36 @@ int main()
 {
     int num, i;
     // Esta variable se usar como flag (bandera)
-    // Mientras vale 1 (true), es que el número es primo. 
+    // Mientras vale 1 (true), es que el nÃºmero es primo. 
     // Si vale 0 (false), no lo es. 
-	int esPrimo = 1;
+    int esPrimo = 1;
 
-    // Solicitar el número al usuario
+    // Solicitar el nÃºmero al usuario
     printf("Ingrese un numero entero positivo: ");
     scanf("%i", &num);
 
-    // Verificar divisibilidad desde 2 hasta la raíz cuadrada del número
+    // Verificar divisibilidad desde 2 hasta la raÃ­z cuadrada del nÃºmero
     for (i = 2; i < num; i++) 
-	{
-		if(num % i == 0)
-		{	
-		    // Si encuentra un divisor, esPrimo pasa a valer 0
-		    // Nunca podrá valer otra cosa, porque multiplcar por 0
-		    // siempre da 0
-			esPrimo = esPrimo * 0; 	
-		}
-		else
-		{
-			esPrimo = esPrimo * 1; 	
-		}        
+    {
+        if(num % i == 0)
+        {    
+            // Si encuentra un divisor, esPrimo pasa a valer 0
+            // Nunca podrÃ¡ valer otra cosa, porque multiplcar por 0
+            // siempre da 0
+            esPrimo = esPrimo * 0;     
+        }
+        else
+        {
+            esPrimo = esPrimo * 1;     
+        }        
     }
     // Mostrar resultado
     if (esPrimo) 
-	{
+    {
         printf("%i es un numero primo.\n", num);
     } 
-	else 
-	{
+    else 
+    {
         printf("%i NO es un numero primo.\n", num);
     }
 
