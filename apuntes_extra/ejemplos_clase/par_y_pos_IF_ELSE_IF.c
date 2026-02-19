b212381998e6ff34aa201e813f86705dfc73f7a4
davizinho5 <dalvar_5@yahoo.es>
2026-02-19 15:00:45 +0100

    EJemplos if else

---
+++ apuntes_extra/ejemplos_clase/par_y_pos_IF_ELSE_IF.c
@@ -0,0 +1,41 @@
+# include <stdio.h>
+
+int main ()
+{
+	int num, resto;
+	
+	printf("Escribe un número entero\n");
+	scanf("%d", &num);
+	
+	// Para comprobar si es par, el resto será 0
+	resto = num % 2;
+	
+	if (num != 0)
+	{
+		if (resto % 2 == 0)
+		{
+			if (num > 0) 
+			{
+				printf("El numero %d es par y positivo.\n", num);
+			}
+			else{
+				printf("El numero %d es par y negativo.\n", num);
+			}
+			
+		}
+		else if (num > 0) 
+		{
+			printf("El numero %d es impar y positivo.\n", num);
+		}
+		else{
+				printf("El numero %d es impar y negativo.\n", num);
+		}
+	}
+	else{
+		printf("Ha escrito el numero cero y no le puedo contestar.\n");
+	}
+	
+	return 0;
+}
+	
+
